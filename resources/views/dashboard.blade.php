ccd885a219dc6581d3047cb0054832998d9becbd
MMaulanaAL <maulanaalf07@gmail.com>
2021-04-15 15:30:02 +0100

    first commit

---
+++ resources/views/dashboard.blade.php
@@ -0,0 +1,137 @@
+<!doctype html>
+<html lang="en">
+
+<head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <!-- Bootstrap CSS -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-BmbxuPwQa2lc/FVzBcNJ7UAyJxM6wuqIj61tLrc4wSX0szH/Ev+nYRRuWlolflfl" crossorigin="anonymous">
+
+    <title>Dashboard Page</title>
+</head>
+
+<body>
+    <style>
+        body {
+            background-image: url('gambar_noname.jpg');
+        }
+    </style>
+
+
+    <nav class="navbar navbar-expand-lg navbar-light bg-warning">
+        <div class="container">
+            <a class="navbar-brand fw-bolder" href="#">Inventario</a>
+            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse" id="navbarNav">
+                <ul class="navbar-nav ms-auto">
+                    <li class="nav-item fw-bolder">
+                        <a class="nav-link active" aria-current="page" href="{{ url('dashboard')}}">Dashboard</a>
+                    </li>
+                    <li class="nav-item fw-bolder">
+                        <a class="nav-link" href="{{ url('user')}}">User</a>
+                    <li class="nav-item fw-bolder">
+                        <a class="nav-link" href="{{ url('barang')}}">Barang</a>
+                    <li class="nav-item fw-bolder">
+                        <a class="nav-link" href="{{ url('laporan')}}">Laporan</a>
+                    <li class="nav-item dropdown ms-3-md">
+                        <a class="nav-link dropdown-toggle fw-bolder" href="#" id="navbarDropdownMenuLink" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                            {{auth()->user()->name}}
+                        </a>
+
+                        <ul class="dropdown-menu fw-bolder" aria-labelledby="navbarDropdownMenuLink">
+                            <li><a class="dropdown-item text-center fw-bolder" href="{{ url('/logout') }}">Logout</a></li>
+                        </ul>
+                    </li>
+                    </li>
+                </ul>
+            </div>
+        </div>
+    </nav>
+
+    <section>
+        <div class="container">
+            <div class="row mt-5 justify-content-center">
+                <div class="col-5">
+                    <marquee direction="left" scrollamount="10">
+                        <h1 class="text-center fw-bolder mt-5">Selamat Datang</h1>
+                        <style>
+                            h1 {
+                                color: lightcoral;
+
+                            }
+                        </style>
+                        <h1 class="text-center fw-bolder">{{auth()->user()->name}}</h1>
+                    </marquee>
+                </div>
+            </div>
+        </div>
+    </section>
+
+
+    <div class="container">
+        <div class="row mt-5 justify-content-center">
+            <div class="col-16 mt-5">
+                <table class="table table-striped table-hover mt-5">
+
+
+                    <tr class="text-center">
+                        <style>
+                            tr {
+                                border-bottom: 50px blue;
+                            }
+                        </style>
+
+                        <th>No</th>
+
+                        <th>Nama</th>
+                        <th>Jumlah</th>
+                        <th>Hasil</th>
+
+
+                    </tr>
+
+
+                    <tr class="table-success text-center">
+                        <th>1</th>
+                        <td> User </td>
+                        <td>{{ $user }}</td>
+                        <th><a href="{{'user'}}"> <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-caret-right-square-fill" viewBox="0 0 16 16">
+                                    <path d="M0 2a2 2 0 0 1 2-2h12a2 2 0 0 1 2 2v12a2 2 0 0 1-2 2H2a2 2 0 0 1-2-2V2zm5.5 10a.5.5 0 0 0 .832.374l4.5-4a.5.5 0 0 0 0-.748l-4.5-4A.5.5 0 0 0 5.5 4v8z" />
+                                </svg></th>
+
+                    </tr>
+                    <tr class="table-success text-center">
+                        <th>2</th>
+                        <td>Barang</td>
+                        <td>{{ $barang }}</td>
+                        <th><a href=" {{'barang'}}"><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-caret-right-square-fill" viewBox="0 0 16 16">
+                                    <path d="M0 2a2 2 0 0 1 2-2h12a2 2 0 0 1 2 2v12a2 2 0 0 1-2 2H2a2 2 0 0 1-2-2V2zm5.5 10a.5.5 0 0 0 .832.374l4.5-4a.5.5 0 0 0 0-.748l-4.5-4A.5.5 0 0 0 5.5 4v8z" />
+                                </svg></th>
+                    </tr>
+            </div>
+        </div>
+    </div>
+    </table>
+
+
+
+
+
+
+
+
+
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta2/dist/js/bootstrap.bundle.min.js" integrity="sha384-b5kHyXgcpbZJO/tY9Ul7kGkf1S0CWuKcCD38l8YkeH8z8QjE0GmW1gYU5S9FOnJ0" crossorigin="anonymous"></script>
+
+    <!-- Option 2: Separate Popper and Bootstrap JS -->
+    <!--
+    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.6.0/dist/umd/popper.min.js" integrity="sha384-KsvD1yqQ1/1+IA7gi3P0tyJcT3vR+NdBTt13hSJ2lnve8agRGXTTyNaBYmCR/Nwi" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta2/dist/js/bootstrap.min.js" integrity="sha384-nsg8ua9HAw1y0W1btsyWgBklPnCUAFLuTMS2G72MMONqmOymq585AcH49TLBQObG" crossorigin="anonymous"></script>
+    -->
+</body>
+
+</html>
